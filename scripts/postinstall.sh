462c5e7ff6a16b13260641a21d2e604a11828f62
Ilya Rogov <ilyar.software@gmail.com>
2016-12-12 20:40:31 +0200

    Fix hack script

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -6,8 +6,8 @@ viewVersion='master'
 dataVersion='master'
 
 cd ${vendorPath}
-git clone --single-branch -b master git@github.com:bem-site/bem-lib-site-view.git
-git clone --single-branch -b master git@github.com:bem-site/bem-lib-site-data.git
+git clone --single-branch -b master https://github.com/bem-site/bem-lib-site-view.git
+git clone --single-branch -b master https://github.com/bem-site/bem-lib-site-data.git
 
 cd ${vendorPath}'/bem-lib-site-view' && git checkout ${viewVersion} && npm install
 cd ${vendorPath}'/bem-lib-site-data' && git checkout ${dataVersion} && npm install
