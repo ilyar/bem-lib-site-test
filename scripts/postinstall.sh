7955aa4cfcc6e4ec90fab75782c3869222349744
Ilya Rogov <ilyar.software@gmail.com>
2016-12-12 19:47:39 +0200

    Update hack

---
+++ scripts/postinstall.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+vendorPath=$(pwd)'/node_modules'
+
+viewVersion='master'
+dataVersion='master'
+
+cd ${vendorPath}
+git clone --single-branch -b master git@github.com:bem-site/bem-lib-site-view.git
+git clone --single-branch -b master git@github.com:bem-site/bem-lib-site-data.git
+
+cd ${vendorPath}'/bem-lib-site-view' && git checkout ${viewVersion} && npm install
+cd ${vendorPath}'/bem-lib-site-data' && git checkout ${dataVersion} && npm install
